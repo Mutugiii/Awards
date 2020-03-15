4cfd2654b9a14a699479d677c6fb1b9bdfd0feaa
mutugiii <mutugimutuma@gmail.com>
2020-03-15 09:54:38 +0300

    Create profile & post models | Perform migrationss

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Projects(models.Model):
+    '''Model class for Projects tha user posts'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='project')
+    title = models.CharField(max_length=50)
+    project_image = CloudinaryField('image')
+    description = models.TextField()
+    live_link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} project {}'.format(self.user.username, self.title)
+
+class Profile(models.Model):
+    '''Model class for User profile'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('image')
+    profile_bio = models.TextField()
+    contact_info = models.EmailField()
+    projects = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
