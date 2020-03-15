cd51532a0e74bd7d082c6a71cab0790c2b267bcc
mutugiii <mutugimutuma@gmail.com>
2020-03-15 19:22:41 +0300

    Alter Profile & Post Api views & Urls

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from rest_framework_jwt.views import obtain_jwt_token
 from .views import *
 
 urlpatterns = [
-    path('tokenauth/',obtain_jwt_token),
     path('currentuser/', get_current_user),
+    path('login/',obtain_jwt_token),
     path('register/', RegisterUser.as_view()),
-    path('projects/', ProjectsListCreateView.as_view(), name='projects'),
+    path('projects/', ProjectsListView.as_view(), name='projects'),
     path('project/<int:pk>', ProjectsDetailView.as_view(), name='specificproject'),
-    path('profiles/', UserProfileListCreateView.as_view(), name='profiles'),
-    path('profile/<int:pk>', UserProfileDetailView.as_view(), name='userprofile'),
+    path('profile/', ProfileListView.as_view(), name='createprofile'),
+    path('profile/<int:id>', ProfileDetailView.as_view(), name='specificprofle'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,16 +2,17 @@ from .serializers import GetUserSerializer, PostUserSerializer, UserProfileSeria
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import permissions, status
 from rest_framework.permissions import IsAuthenticated
 from .models import Profile, Project
+from .permissions import IsOwnerProfileOrReadOnly
+from django.shortcuts import Http404
 
 @api_view(['GET'])
 def get_current_user(request):
     '''Endpoint to Authenticate & fetch user information'''
-    serializer = GetUserSerializer(request.user)
-    return Response(serializer.data)
+    serializers = GetUserSerializer(request.user)
+    return Response(serializers.data)
 
 class RegisterUser(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -21,43 +22,91 @@ class RegisterUser(APIView):
         user = request.data.get('user')
         if not user:
             return Response({'response' : 'error', 'message' : 'No data found'})
-        serializer = PostUserSerializer(data=user)
-        if serializer.is_valid():
-            saved_user = serializer.save()
+        serializers = PostUserSerializer(data=user)
+        if serializers.is_valid():
+            saved_user = serializers.save()
         else:
-            return Response({'response':'error', 'message': serializer.errors})
+            return Response({'response':'error', 'message': serializers.errors})
         return Response({'response':'success','message':'User Created Successfully'})
 
-class UserProfileListCreateView(ListCreateAPIView):
-    '''View to get all the profiles and create a new profile'''
-    queryset = Profile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        '''Populate read-only user with the request.user'''
-        user = self.request.user
-        serializer.save(user=user)
-
-class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
-    '''View details of a users specific profile'''
-    queryset = Profile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsOwnerProfileOrReadOnly,IsAuthenticated]
-
-class ProjectsListCreateView(ListCreateAPIView):
+class ProjectsListView(APIView):
     '''View to get all the Projects and create a new Project'''
-    queryset = Project.objects.all()
-    serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        '''Populate read-only user with the request.user'''
-        user = self.request.user
-        serializer.save(user=user)
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = UserProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
 
-class ProjectsDetailView(RetrieveUpdateDestroyAPIView):
+    def post(self, request, format=None):
+        serialiazers = UserProjectSerializer(data=request.data)
+        if serialiazers.is_valid():
+            serialiazers.save()
+            return Response(serialiazers.data, status=status.HTTP_201_CREATED)
+        return Response(serialiazers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectsDetailView(APIView):
     '''View all the details of a project'''
-    queryset = Project.objects.all()
-    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_project(self, id):
+        try:
+            return Project.objects.get(pk=id)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, id, format=None):
+        project = get_project(id)
+        serializers = UserProjectSerializer(project)
+        return Response(serializers.data)
+
+    def put(self, request, id, format=None):
+        project = get_project(id)
+        serializers = UserProfileSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id, format=None):
+        project = get_project(id)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ProfileListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        '''Function to add to a profile'''
+        serializers = UserProfileSerializer(data=request.data)        
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileDetailView(APIView):
+    '''View function for the user profile'''
     permission_classes = [IsOwnerProfileOrReadOnly,IsAuthenticated]
+    def get_profile(self, id):
+        try:
+            return Profile.objects.get(pk=id)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, id, format=None):
+        profile = self.get_profile(id)
+        serializers = UserProfileSerializer(profile)
+        return Response(serializers.data)
+
+    def put(self, request, id, format=None):
+        profile = self.get_profile(id)
+        serializers = UserProfileSerializer(profile, request.data)
+        if serializers.is_valid():
+            serializers.save()            
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SearchProfileListView(APIView):
+    pass
